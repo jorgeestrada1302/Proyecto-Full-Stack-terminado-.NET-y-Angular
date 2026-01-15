6da7d2b6951d819f53e639bdda50639fef40f5e4
Jorge <estradaunitrabajos@gmail.com>
2026-01-15 15:51:32 -0600

    Corregir nombre ApplicationDbContext

--- IncidentesApi/Program.cs
+++ IncidentesApi/Program.cs
@@ -1,40 +1,42 @@
+
 using IncidentesApi.Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// 1. Configurar la conexión a SQL Server
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+// 1. Configurar conexión a BD
+// (Aquí arreglamos el error de la línea 8 que se te cortó)
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") 
+                       ?? Environment.GetEnvironmentVariable("ConnectionStrings__DefaultConnection");
 
-// 2. Configurar CORS (ESTA PARTE ES LA QUE TENIAS MAL)
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("PermitirAngular", policy =>
-    {
-        policy.AllowAnyOrigin()
-            .AllowAnyMethod()
-            .AllowAnyHeader();
-    });
-});
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// 2. CONFIGURAR CORS (Permitir todo)
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("PermitirTodo", policy => 
+    {
+        policy.AllowAnyOrigin()    // Acepta cualquier IP
+            .AllowAnyHeader()    // Acepta cualquier encabezado
+            .AllowAnyMethod();   // Acepta GET, POST, PUT, DELETE
+    });
+});
+
 var app = builder.Build();
 
-// Configurar el pipeline HTTP
+// 3. USAR CORS (¡IMPORTANTE! Debe ir ANTES de MapControllers)
+app.UseCors("PermitirTodo");
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-app.UseHttpsRedirection();
-
-// 3. Activar los permisos para Angular
-app.UseCors("PermitirAngular");
 
 app.UseAuthorization();
 
